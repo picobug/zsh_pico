97d8a45c4d777ccc57bafdf45c40dea22543ac80
Julian Prasetyo <picobug.jp@gmail.com>
2020-02-26 14:28:49 +0700

    Update Sampah

--- picobug.zsh
+++ picobug.zsh
@@ -57,3 +57,7 @@ kubeup(){
   curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && mv minikube ~/.bin/minikube
   curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && mv kubectl ~/.bin/kubectl
 }
+sampah() {
+  rm -rfv "$HOME/.config/VSCodium/Cache"
+  ln -s /tmp/picobug "$HOME/.config/VSCodium/Cache"
+}
